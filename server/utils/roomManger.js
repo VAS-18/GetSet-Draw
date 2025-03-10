617d2a2bd599ecb5f5dcabc2e4c911856d175cf2
VAS-18 <uday.vas18@gmail.com>
2025-03-11 02:40:39 +0530

    implement room joining logic; add checks for existing players and room capacity

--- server/utils/roomManger.js
+++ server/utils/roomManger.js
@@ -1,4 +1,4 @@
-// import { Room } from "../models/Room.js";
+import { Room } from "../models/Room.js";
 // import { v4 as uuidv4 } from 'uuid';
 
 const rooms = new Map();
@@ -19,6 +19,8 @@ function generateRoomCode() {
     // console.log(length);
 }
 
+
+//function to generate rooms
 async function generateRoom(socketId) {
     if (socketRooms.has(socketId)) {
         const existingRoom = socketRooms.get(socketId);
@@ -34,6 +36,7 @@ async function generateRoom(socketId) {
     roomCode = generateRoom();
     isUnique = false;
 
+    //checks if the generated room is unique or not
     while (!isUnique) {
         roomCode = generateRoom();
         if (rooms.get(roomCode)) {
@@ -61,8 +64,56 @@ async function generateRoom(socketId) {
 }
 
 
+//function to join rooms
 async function joinRoom(socketId, roomCode) {
-    if(socketRooms.has(socketId)){
+    if (socketRooms.has(socketId)) {
         console.log(`${socketId} already present in the room, exiting...`);
     }
+
+    const room = rooms.get(roomCode);
+
+    if (!room) {
+        console.log(`Room not found`);
+        return {
+            success: false,
+            roomCode
+        }
+    }
+
+    //Check for Existing players
+    const ExistingPlayer = room.players.some(player => player.id === socketId);
+
+    if (ExistingPlayer) {
+        console.log(`Player ${socketId} already exits in room ${roomCode}`);
+        return {
+            success: false,
+            roomCode
+        }
+    }
+
+    //Check to see if the room is full or not 
+
+    const roomSize = room.player.length;
+
+    if (roomSize >= 2) {
+        console.log(`Room ${roomCode} is already full`);
+        return {
+            success: false,
+            message: "Room is full",
+            roomCode
+        }
+    }
+
+    //Adding new players to the room
+    room.players.push({
+        id: socketId,
+        ready: false
+    });
+
+    socketRooms.set(socketId, roomCode);
+
+    console.log(`Player ${socketId} joined room: ${roomCode}, current players:`, room.players.map(p => p.id));
+
+    return { success: true, roomCode, room };
+
 }
